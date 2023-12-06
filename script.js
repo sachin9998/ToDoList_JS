8748ae507cd8022ec65b0ea0fb8a109101725f9d
Sachin Alam <sachinalam4216@gmail.com>
2023-12-06 18:14:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+// Adding task function
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+
+  inputBox.value = "";
+  saveData();
+}
+
+// click function javascript
+listContainer.addEventListener(
+  "click",
+  (event) => {
+    if (event.target.tagName == "LI") {
+      event.target.classList.toggle("checked");
+      saveData();
+    } else if (event.target.tagName == "SPAN") {
+      event.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
